feb2feb35c5af5b7e79f51e8a1e00d38ce4a1502
ManojSynectiks <manoj.sharma@synectiks.com>
2023-08-21 11:48:38 +0530

    condition fix in appconfig api

--- handlers/appconfig-handler.go
+++ handlers/appconfig-handler.go
@@ -11,36 +11,62 @@ import (
 func GetAppconfig(w http.ResponseWriter, r *http.Request) {
 	log.Info("Starting GetAppconfig api")
 
-	vaultUrl := r.URL.Query().Get("vaultUrl")
-	if vaultUrl == "" {
-		log.Error("VaultUrl not provided")
-		http.Error(w, fmt.Sprintf("VaultUrl not provided"), http.StatusBadRequest)
-		return
-	}
-
-	accountId := r.URL.Query().Get("accountId")
-	if accountId == "" {
-		log.Error("AccountId not provided")
-		http.Error(w, fmt.Sprintf("AccountId not provided"), http.StatusBadRequest)
-		return
-	}
-
 	region := r.URL.Query().Get("zone")
 	if region == "" {
-		log.Error("Region/Zone not provided")
-		http.Error(w, fmt.Sprintf("Region/Zone not provided"), http.StatusBadRequest)
+		log.Error("Zone(Region) not provided")
+		http.Error(w, fmt.Sprintf("Zone(Region) not provided"), http.StatusBadRequest)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
-	result, err := cmd.GetCloudConfigSummary(region, vaultUrl, accountId)
-	if err != nil {
-		log.Error("Exception in getting cloud config summary: %v", err)
-		http.Error(w, fmt.Sprintf("Exception in getting cloud config summary"), http.StatusInternalServerError)
-	}
-	json.NewEncoder(w).Encode(result)
 
-	//json.NewEncoder(w).Encode(cmd.GetConfig(raw.AccessDetails.Region, raw.AccessDetails.CrossAccountRoleArn, raw.AccessDetails.AccessKey, raw.AccessDetails.SecretKey, raw.AccessDetails.ExternalId))
+	vaultUrl := r.URL.Query().Get("vaultUrl")
+
+	if vaultUrl != "" {
+		accountId := r.URL.Query().Get("accountId")
+		if accountId == "" {
+			log.Error("AccountId not provided")
+			http.Error(w, fmt.Sprintf("AccountId not provided"), http.StatusBadRequest)
+			return
+		}
+
+		result, err := cmd.GetCloudConfigSummary(region, vaultUrl, accountId)
+		if err != nil {
+			log.Error("Exception in getting cloud config summary: %v", err)
+			http.Error(w, fmt.Sprintf("Exception in getting cloud config summary"), http.StatusInternalServerError)
+		}
+		json.NewEncoder(w).Encode(result)
+	} else {
+		accessKey := r.URL.Query().Get("accessKey")
+		if accessKey == "" {
+			log.Error("AccessKey not provided")
+			http.Error(w, fmt.Sprintf("AccessKey not provided"), http.StatusBadRequest)
+			return
+		}
+		secretKey := r.URL.Query().Get("secretKey")
+		if secretKey == "" {
+			log.Error("SecretKey not provided")
+			http.Error(w, fmt.Sprintf("SecretKey not provided"), http.StatusBadRequest)
+			return
+		}
+		crossAccountRoleArn := r.URL.Query().Get("crossAccountRoleArn")
+		if crossAccountRoleArn == "" {
+			log.Error("CrossAccountRoleArn not provided")
+			http.Error(w, fmt.Sprintf("CrossAccountRoleArn not provided"), http.StatusBadRequest)
+			return
+		}
+		externalId := r.URL.Query().Get("externalId")
+		if externalId == "" {
+			log.Error("ExternalId not provided")
+			http.Error(w, fmt.Sprintf("ExternalId not provided"), http.StatusBadRequest)
+			return
+		}
+		result, err := cmd.GetConfig(region, crossAccountRoleArn, accessKey, secretKey, externalId)
+		if err != nil {
+			log.Error("Exception in getting cloud config summary: %v", err)
+			http.Error(w, fmt.Sprintf("Exception in getting cloud config summary"), http.StatusInternalServerError)
+		}
+		json.NewEncoder(w).Encode(result)
+	}
 
 	log.Info("GetAppconfig completed")
 
