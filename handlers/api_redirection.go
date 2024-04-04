125af5acecc885d8628f32b53d937c593b938111
Mohd Mukarramuddin <mohd.mukarramuddin@synectiks.com>
2024-04-04 16:44:47 +0530

    RDS uptime api panel

--- handlers/api_redirection.go
+++ handlers/api_redirection.go
@@ -404,6 +404,9 @@ func ExecuteQuery(w http.ResponseWriter, r *http.Request) {
 	if elementType == "RDS" && query == "maintenance_schedule_overview_panel" {
 		RDS.ScheduleOverviewPanel(w, r)
 	}
+	if elementType == "RDS" && query == "uptime_percentage_panel" {
+		RDS.GetRDSUptimeData(w, r)
+	}
 	if elementType == "ApiGateway" && query == "uptime_percentage_panel" {
 		ApiGateway.GetUptimePercentagePanel(w, r)
 	}
