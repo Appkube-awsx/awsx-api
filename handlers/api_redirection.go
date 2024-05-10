21e1b0ec292ae67f5965f1a0be2a42ff17fe1742
NikithaBoredha <nikitha.boredha@synectiks.com>
2024-05-10 11:40:46 +0530

    added file top_used_functions_panel

--- handlers/api_redirection.go
+++ handlers/api_redirection.go
@@ -324,6 +324,9 @@ func ExecuteQuery(w http.ResponseWriter, r *http.Request) {
 	if elementType == "LAMBDA" && query == "success_and_failed_function_panel" {
 		Lambda.GetSuccessAndFailedFunctionPanel(w, r)
 	}
+	if elementType == "LAMBDA" && query == "top_used_functions_panel" {
+		Lambda.GetTopUsedFunctionsPanel(w, r)
+	}
 	if elementType == "RDS" && query == "cpu_utilization_panel" {
 		RDS.GetCpuUtilizationPanel(w, r)
 	}
